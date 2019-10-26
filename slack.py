ba1bddfe3f5afe524794eb931caec0ca59c74994
Daniel Kong <danielkong365@gmail.com>
2019-10-26 14:58:01 -0400

    Add dotenv requirements

--- slack.py
+++ slack.py
@@ -1,9 +1,21 @@
 from flask import Flask, jsonify, request
 
+import os
+import dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+dotenv.load_dotenv(dotenv_path)
+verification_token = os.environ['VERIFICATION_TOKEN']
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    return 'Hello, World!'
+
+
+@app.route('/boost')
+def boost():
     return 'Hello, World!'
 
 if __name__ == '__main__':
